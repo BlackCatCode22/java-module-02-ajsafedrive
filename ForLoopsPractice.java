3847daf505ff042418cb778550383b339032a76c
ajsafedrive <156267981+ajsafedrive@users.noreply.github.com>
2025-08-26 11:44:05 -0700

    Add files via upload

---
+++ ForLoopsPractice.java
@@ -0,0 +1,14 @@
+// aJ ForLoopsPractice.java
+// 8/26/25
+
+public class ForLoopsPractice {
+    public static void main(String[] args) {
+
+        // A loop must have a loop control variables
+        // A loop control variable must be initialized, checked, and changed.
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
