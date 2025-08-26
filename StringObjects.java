febeb0ef92531ea1ceff0ef0e558d7f3be41c602
ajsafedrive <156267981+ajsafedrive@users.noreply.github.com>
2025-08-26 16:10:20 -0700

    Add files via upload

---
+++ StringObjects.java
@@ -0,0 +1,16 @@
+// aJ StringObjects.java
+// 8/26/25
+
+public class StringObjects {
+    public static void main(String[] args) {
+
+        String phrase = "Struggle City";
+        System.out.println(phrase.substring(0,phrase.indexOf(" ")));
+        System.out.println("This is my uppercase test " + phrase.toUpperCase());
+        System.out.println("This is my lowercase test" + phrase.toLowerCase());
+        System.out.println("This is my char test" + phrase.charAt(5));
+
+
+
+    }
+}
