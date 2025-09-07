1a31e8823b437bc04b2b714576ca817d59383d54
ajsafedrive <156267981+ajsafedrive@users.noreply.github.com>
2025-09-07 13:30:29 -0700

    Add files via upload

---
+++ ForLoopStuff.java
@@ -0,0 +1,34 @@
+// aj 9/4/25
+// Playing with for loops
+// ForLoopStuff.java
+
+
+public class ForLoopStuff {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome! \n\n");
+
+        String[] cars = {"Toyota Corolla", "Honda Civic", "Nissan Altima"};
+
+        // my for loop
+        //for(int i = 0; i < cars.length; i++) {
+            //System.out.println(cars[i]);
+
+
+            // My for each loop
+        for(String car : cars) {
+            System.out.println(car);
+
+
+
+
+
+            // This is code I wanted to keep, but no longer is relevant to current method.
+            //    for(char car : cars) {
+            //    System.out.println(car);
+
+        }
+
+        }
+
+        }
+
